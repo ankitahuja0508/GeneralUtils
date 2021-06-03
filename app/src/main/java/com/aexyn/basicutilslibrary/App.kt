bd95fc1516a878092191d1a2d560b17aa74ff5c0
ankitahuja007@gmail.com <ankitahuja007@gmail.com>
2021-06-03 14:11:15 +0530

    fix devutils

--- app/src/main/java/com/aexyn/basicutilslibrary/App.kt
+++ app/src/main/java/com/aexyn/basicutilslibrary/App.kt
@@ -1,6 +1,11 @@
 package com.aexyn.basicutilslibrary
 
+import DevUtils
 import android.app.Application
 
 class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        DevUtils.initFlipper(this)
+    }
 }
